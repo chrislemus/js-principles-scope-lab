8a34d140d757afae8123bc64696f3d4ec66448e3
chrislemus <icrislemus@gmail.com>
2021-03-15 12:22:03 -0400

    commit tree 83357cbac82d316397d94659ce97fc4c5abe38d0

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+}
+
+
+function setBestCustomer() {
+  window.bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+  window.bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = ''
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 's'
+}
